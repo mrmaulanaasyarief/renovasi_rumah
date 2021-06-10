db4303dca12b2a0956184e378a16453d542f13a1
Muhammad Ramadhan <mr.maulanaasyarief@gmail.com>
2021-06-10 22:06:53 +0700

    change database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'renovasi_rumah',
+		'database' => 'renovasi_rumah_material',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
